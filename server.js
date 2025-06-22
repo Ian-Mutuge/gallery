893f3056e2788c13b7cf44d3005b396fdd13ba84
Ian-Mutuge <mvtuge@gmail.com>
2025-06-22 18:25:36 +0300

    LocalCloud flexibility reverted

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-require('dotenv').config();
+//require('dotenv').config();
 
 // Define routes
 let index = require('./routes/index');

ed6688f5269dcb98cb0d57c347311781ec814f2e
Ian-Mutuge <mvtuge@gmail.com>
2025-06-22 20:16:43 +0300

    Testing locally

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ let image = require('./routes/image');
 
 // Initializing the app
 const app = express();
+let mongodb_url = 'mongodb://localhost:27017/';
+let dbName = 'darkroom';
 
 // connecting the database
 
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
+const MONGODB_URI = `${mongodb_url}${dbName}`||process.env.MONGODB_URI || config.mongoURI[app.settings.env]
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
     if (err) {
         console.log(err)

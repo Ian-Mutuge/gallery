21f6413b134206c29877e7f64d94ffe64ae741a1
Ian-Mutuge <mvtuge@gmail.com>
2025-06-23 19:28:22 +0300

    Steps to success

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const config = require('./config');
+const config = require('./_config');
 
 //require('dotenv').config();
 

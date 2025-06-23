345eafeecaeac409d55de78b94db8527401ce69f
Ian-Mutuge <mvtuge@gmail.com>
2025-06-23 18:26:48 +0300

    Testing it works first

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ let dbName = 'darkroom';
 
 // connecting the database
 
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
+const MONGODB_URI = process.env.MONGODB || config.mongoURI[app.settings.env]
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
     if (err) {
         console.log(err)
